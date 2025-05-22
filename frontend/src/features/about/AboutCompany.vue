3e3940f9bd26f20ce26cf77140cc82ebcae1aa6b
jhordev <jhordymondragon4@gmail.com>
2025-05-22 17:05:33 -0500

    PC14: Cambios en about view

--- frontend/src/features/about/AboutCompany.vue
+++ frontend/src/features/about/AboutCompany.vue
@@ -3,8 +3,19 @@
 </script>
 
 <template>
-    <section class="">
-
+    <section class="py-[36px] md:py-[80px]  bg-blueOscuro/5 dark:bg-blueOscuro">
+       <div class="container-general grid grid-cols-12  gap-7 items-center">
+           <div class="col-span-12 md:col-span-7">
+               <h2 class="text-textColor mb-4 dark:text-white text-[40px] font-bold">Nosotros</h2>
+               <p class="text-textColor md:max-w-160 dark:text-white  text-[18px] font-normal">
+                   Somos un sitio web local especializado en la publicación de motos y autos que abarca principalmente las ciudades de Moroleón, Uriangato y Yuriria en el estado de Guanajuato y algunos alrededores muy cercanos.
+               </p>
+           </div>
+           <div class="col-span-5 relative hidden md:block">
+               <div class="w-full h-full bg-principalcolor/10  rounded-3xl absolute z-0 -top-5 -right-5"></div>
+               <img src="@/assets/venta.jpg" class="rounded-3xl relative z-10" alt="Venta de Moto"/>
+           </div>
+       </div>
     </section>
 </template>
 
--- frontend/src/views/AboutUsView.vue
+++ frontend/src/views/AboutUsView.vue
@@ -3,13 +3,14 @@ import Footer from "@/components/composite/Footer.vue";
 import Header from "@/components/composite/Header.vue";
 import PageHeader from "@/components/composite/PageHeader.vue";
 import AboutCompany from "@/features/about/AboutCompany.vue";
+import MisionVision from "@/features/about/MisionVision.vue";
 </script>
 
 <template>
     <Header header-type="inline"/>
-    <main class="mt-16 md:mt-20 container-general">
-        <PageHeader title="Sobre Nosotros" current="Sobre Nosotros" />
+    <main class="mt-16 md:mt-20">
         <AboutCompany/>
+        <MisionVision/>
     </main>
     <Footer/>
 </template>
